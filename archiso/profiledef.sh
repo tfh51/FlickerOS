5d55205b69cc4d6e507e804c21c6bcff22aa7fcf
tfh51 <168468757+tfh51@users.noreply.github.com>
2024-05-02 00:19:04 +0500

    FlickerOS Beginning

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -1,32 +1,34 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
+iso_name="carli"
+iso_label="carli-$(date +%Y%m)"
+iso_publisher="Carli Linux <https://www.arcolinuxiso.com>"
+iso_application="Carli Linux Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
-           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
+           'uefi-ia32.systemd-boot.esp' 'uefi-x64.systemd-boot.esp'
+           'uefi-ia32.systemd-boot.eltorito' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
+  ["/etc/gshadow"]="0:0:400"	
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/root/.gnupg"]="0:0:700"
+  ["/etc/polkit-1/rules.d"]="0:0:750"
+  ["/etc/sudoers.d"]="0:0:750"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
-  ["/etc/polkit-1/rules.d"]="0:0:750"
-  ["/etc/sudoers.d"]="0:0:750"
-  ["/usr/local/bin/alci-make-a-pure-arch"]="0:0:755"
-  ["/usr/local/bin/alci-displaymanager-check"]="0:0:755"
-  ["/usr/local/bin/get-nemesis-on-alci"]="0:0:755"
-  ["/usr/local/bin/fix-keys"]="0:0:755"
+  ["/usr/local/bin/carli-before"]="0:0:755"
+  ["/usr/local/bin/carli-final"]="0:0:755"
+  ["/usr/local/bin/get-nemesis-on-carli"]="0:0:755"
+  ["/usr/local/bin/fix-keys"]="0:0:755" 
 )
